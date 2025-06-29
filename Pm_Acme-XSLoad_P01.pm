c0273504c896975dbc52a0bc366000a88f927844
Klaus Eichner <klaus03@gmail.com>
2025-06-29 11:41:08 +0200

    Update Pm_Acme-XSLoad_P01.pm...

--- Pm_Acme-XSLoad_P01.pm
+++ Pm_Acme-XSLoad_P01.pm
@@ -1,3 +1,7 @@
+use 5.032;
+use warnings;
+no warnings qw(once);
+
 package Acme::XSLoad;
 
 if (defined(&DynaLoader::boot_DynaLoader) and !defined(&DynaLoader::dl_error)) {
@@ -22,6 +26,38 @@ sub load_dll {
     @DynaLoader::dl_require_symbols = ($bootname);
 
     my $boot_symbol_ref;
+
+    # Here we are loading the *.dll file:
+    # Be advised that some rust *.dll's can have a potential dependency on other *.dll's that
+    # live in the directory C:\Users\<name>\.rustup\toolchains\stable-x86_64-pc-windows-gnu\bin\
+    # That is particularely true if the rust *.dll has been compiled with "rustc -C prefer-dynamic".
+    #
+    # There are 4 *.dll's in that directory:
+    #
+    #   1.) libgcc_s_seh-1.dll                       141_824 bytes
+    #   2.) libwinpthread-1.dll                       53_760 bytes
+    #   3.) rustc_driver-bf80254675d53dbd.dll    271_605_611 bytes
+    #   4.) std-05f2cf19e11e0135.dll              15_162_247 bytes
+    #
+    # These 4 *.dll's should be copied into a new, separate directory, for example
+    # C:\Users\<name>\Documents\Ext\FileRust\ and this new directory should be added to the %PATH%.
+    #
+    # Here is a commandline tool that lets you inspect the dependecies of a particular *.dll
+    # Source: https://github.com/lucasg/Dependencies/releases
+    #
+    # Be advised that, for one single *.dll, the program runs for several minutes and the
+    # output typically is several megabytes in size and more than 10_000 lines of text.
+    #
+    # C:\> Dependencies.exe -chain csv_get.dll >dump.txt
+    # C:\> perl -nE "next unless /NOT_FOUND/; s/\A[\s\|\x{c3}]*//; next if /\Aext-ms-/ or /\Aapi-ms-/; print" <dump.txt
+    #
+    # Here is a sample ouput :
+    #
+    # UpdateAPI.dll (NOT_FOUND) :
+    # PdmUtilities.dll (NOT_FOUND) :
+    # HvsiFileTrust.dll (NOT_FOUND) :
+    # HvsiFileTrust.dll (NOT_FOUND) :
+    # HvsiFileTrust.dll (NOT_FOUND) :
 
     my $libref = DynaLoader::dl_load_file($file, 0)
       or die "Can't load '$file' for module $module: ", DynaLoader::dl_error();
